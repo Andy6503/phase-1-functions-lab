789b95d5a1911e5ac35707b0ecfd44f2186f0d3e
Andrew <andrew.kapsa1@gmail.com>
2022-01-12 18:23:20 -0500

    commit tree 24c0129f9246e9b537eddb101362696c0b307dfa

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks>42){
+    return (blocks-42)
+    }
+    else {
+    return (42 - blocks)
+    }
+
+}
+function distanceFromHqInFeet (blocks){
+    if (blocks>42){
+    return (blocks-42) * 264;
+    }
+    else {
+        return (42-blocks) * 264;
+    }
+}
+function distanceTravelledInFeet(start, end) {
+    if (start < end)  {
+        return (end - start) * 264;
+    } else {
+        return (start - end) * 264;
+    }
+}
+function calculatesFarePrice(start, destination){
+
+    let distance = distanceTravelledInFeet(start, destination);
+    console.log(distance)
+     if (distance<400){
+       return 0;
+     }else if(distance >=400 && distance <=2000){
+       return (distance-400)*0.02;
+   
+     }
+     else if(distance >2000 && distance < 2500){
+     return 25;
+     }
+   
+     else if(distance>2500){
+       return 'cannot travel that far';
+     }
+   
+   }
